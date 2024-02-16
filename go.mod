7e2b795d56d479a9d3637be634f99998dadf894b
John Schaeffer <jschaeffer@equinix.com>
2024-02-16 11:20:27 -0500

    Add tests
    This commit adds tests to iam-runtime-static.
    
    Signed-off-by: John Schaeffer <jschaeffer@equinix.com>

--- go.mod
+++ go.mod
@@ -8,12 +8,14 @@ require (
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.17.0
+	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.26.0
 	google.golang.org/grpc v1.58.3
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -21,6 +23,7 @@ require (
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
---
+++ internal/server/server_test.go
@@ -0,0 +1,159 @@
+package server
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/metal-toolbox/iam-runtime/pkg/iam/runtime/authentication"
+	"github.com/metal-toolbox/iam-runtime/pkg/iam/runtime/authorization"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestServer(t *testing.T) {
+	// Run everything in parallel
+	t.Parallel()
+
+	// Set up a server with a simple policy.
+	subjectAlice := "alice"
+	subjectBob := "bob"
+
+	envVarAlice := "IAM_ALICE_TOKEN"
+	envVarBob := "IAM_BOB_TOKEN"
+
+	tokenAlice := "alic3!"
+	tokenBob := "b0b"
+	tokenDNE := "doesnotexist"
+
+	actionGreet := "greet"
+
+	resourceWorld := "resourc-world"
+
+	// In this policy, Alice has the ability to greet the world, but Bob does not.
+	authPolicy := policy{
+		Subjects: []policySubject{
+			{
+				ID: subjectAlice,
+				Tokens: []policyToken{
+					{
+						EnvVar: envVarAlice,
+					},
+				},
+				Resources: []policyResource{
+					{
+						ID: resourceWorld,
+						Actions: []string{
+							actionGreet,
+						},
+					},
+				},
+			},
+			{
+				ID: subjectBob,
+				Tokens: []policyToken{
+					{
+						EnvVar: envVarBob,
+					},
+				},
+				Resources: []policyResource{},
+			},
+		},
+	}
+
+	// Set the environment variables for the test
+	os.Setenv(envVarAlice, tokenAlice)
+	os.Setenv(envVarBob, tokenBob)
+
+	logger := zap.NewNop().Sugar()
+
+	srv, err := newFromPolicy(authPolicy, logger)
+
+	require.NoError(t, err)
+
+	t.Run("ValidateCredentialSuccess", func(t *testing.T) {
+		t.Parallel()
+
+		req := &authentication.ValidateCredentialRequest{
+			Credential: tokenAlice,
+		}
+		resp, err := srv.ValidateCredential(context.Background(), req)
+
+		require.NoError(t, err)
+		require.Equal(t, authentication.ValidateCredentialResponse_RESULT_VALID, resp.Result)
+		require.Equal(t, subjectAlice, resp.Subject.SubjectId)
+	})
+
+	t.Run("ValidateCredentialFail", func(t *testing.T) {
+		t.Parallel()
+
+		req := &authentication.ValidateCredentialRequest{
+			Credential: tokenDNE,
+		}
+		resp, err := srv.ValidateCredential(context.Background(), req)
+
+		require.NoError(t, err)
+		require.Equal(t, authentication.ValidateCredentialResponse_RESULT_INVALID, resp.Result)
+	})
+
+	t.Run("CheckAccessSuccess", func(t *testing.T) {
+		t.Parallel()
+
+		req := &authorization.CheckAccessRequest{
+			Credential: tokenAlice,
+			Actions: []*authorization.AccessRequestAction{
+				{
+					ResourceId: resourceWorld,
+					Action:     actionGreet,
+				},
+			},
+		}
+		resp, err := srv.CheckAccess(context.Background(), req)
+
+		require.NoError(t, err)
+		require.Equal(t, authorization.CheckAccessResponse_RESULT_ALLOWED, resp.Result)
+	})
+
+	t.Run("CheckAccessUnauthenticated", func(t *testing.T) {
+		t.Parallel()
+
+		req := &authorization.CheckAccessRequest{
+			Credential: tokenDNE,
+			Actions: []*authorization.AccessRequestAction{
+				{
+					ResourceId: resourceWorld,
+					Action:     actionGreet,
+				},
+			},
+		}
+
+		_, err := srv.CheckAccess(context.Background(), req)
+
+		errStatus, ok := status.FromError(err)
+
+		require.Equal(t, true, ok)
+		require.Equal(t, codes.InvalidArgument, errStatus.Code())
+	})
+
+	t.Run("CheckAccessUnauthorized", func(t *testing.T) {
+		t.Parallel()
+
+		req := &authorization.CheckAccessRequest{
+			Credential: tokenBob,
+			Actions: []*authorization.AccessRequestAction{
+				{
+					ResourceId: resourceWorld,
+					Action:     actionGreet,
+				},
+			},
+		}
+
+		resp, err := srv.CheckAccess(context.Background(), req)
+
+		require.NoError(t, err)
+		require.Equal(t, authorization.CheckAccessResponse_RESULT_DENIED, resp.Result)
+	})
+}
